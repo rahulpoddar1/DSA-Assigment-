64dcae45b199726addeadf78612a0a1b4a3a7da9
rahulpoddar1 <132883592+rahulpoddar1@users.noreply.github.com>
2024-08-19 14:58:43 +0545

    Add files via upload

---
+++ dsaAssignment/src/main/java/com/example/dsaassignment/DsaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaAssignmentApplication.class, args);
+    }
+
+}
